27ab7e7b7714edfca92c341123c755a45555da03
goncaloach <goncaloach@gmail.com>
2021-11-15 04:00:26 +0000

    falta inscrever no diretorio

--- src/DataClient.java
+++ src/DataClient.java
@@ -2,10 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class DataClient {
 
-    public DataClient(String adress, int port){
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    public DataClient(String addressText, int port){
+        /*try {
+            InetAddress address = InetAddress.getByName(addressText);
+            Socket socket = new Socket(address, port);
+            in = new ObjectInputStream(socket.getInputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
     private void openGUI() {
@@ -43,10 +59,33 @@ public class DataClient {
         });
     }
 
+    private boolean isIntegerInBounds( String input ) {
+        try {
+            int num = Integer.parseInt(input);
+            if(num<0 || num>999999){
+                System.err.println("Second argument must be a number between 0 and 999999");
+                return  false;
+            }
+            return true;
+        }
+        catch( Exception e ) {
+            System.err.println("Second argument must be a number between 0 and 999999");
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
         if(args.length != 2)
             throw new IllegalArgumentException("The number of arguments must be 2");
+        try{
+            int num = Integer.parseInt(args[1]);
+            DataClient dc = new DataClient(args[0],num);
+            dc.openGUI();
+        }catch( Exception e ) {
+            throw new IllegalArgumentException("Second argument must be a number");
+        }
+
+
         DataClient dc = new DataClient(args[0],Integer.parseInt(args[1]));
         dc.openGUI();
     }
--- src/StorageNode.java
+++ src/StorageNode.java
@@ -1,35 +1,96 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 
 public class StorageNode {
 
     private CloudByte[] cloudBytes = new CloudByte[1000000];
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Socket socket;
+    private int storagePort;
+    private ServerSocket serverSocket;
 
-    public StorageNode(){
-        createCloudBytes();
+    public StorageNode(String serverAddressText, int serverPort, int storagePort, String fileName) throws IOException {
+        this.storagePort = storagePort;
+        serverSocket = new ServerSocket(storagePort);
+        connectToServer(serverAddressText,serverPort,storagePort);
+
+        createCloudBytes(fileName);
+        injectError();
     }
 
-    private void createCloudBytes(){
+    private void connectToServer(String serverAddressText, int serverPort, int storagePort) throws IOException {
+        InetAddress address = InetAddress.getByName(serverAddressText);
+        socket = new Socket(address,serverPort);
+
+        System.out.println("Socket:" + socket);
+
+        //TODO
+        //in = new ObjectInputStream(serverSocket.accept().getInputStream());
+        //in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        //in = new ObjectInputStream(socket.getInputStream());
+        out = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public StorageNode(String serverAddressText, int serverPort, int storagePort){
+        //TODO
+        //injectError();
+    }
+
+    private void createCloudBytes(String fileName){
         try {
-            byte[] fileContents = Files.readAllBytes(new File("data.bin").toPath());
-            for (int i = 0; i < fileContents.length; i++) {
+            byte[] fileContents = Files.readAllBytes(new File(fileName).toPath());
+            for (int i = 0; i < fileContents.length; i++)
                 cloudBytes[i] = new CloudByte(fileContents[i]);
-            }
+            System.out.println("Loaded data from file: "+cloudBytes.length);
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error while reading file");
         }
     }
 
+    private void injectError(){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                String content = reader.readLine();
+                String[] splited = content.split("\\s+");
+                if (splited.length == 2 && splited[0].equals("ERROR")) {
+                    int numByte = Integer.parseInt(splited[1]);
+                    if(numByte<0 || numByte>999999)
+                        throw new IllegalArgumentException("Second argument must be a number between 0 and 999999");
+                    cloudBytes[numByte].makeByteCorrupt();
+                    System.out.println("Injected Error into byte number: " + numByte);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
+    private class DealWithClient extends  Thread{
+
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+
+        public DealWithClient(Socket socket) throws IOException {
+            in = new ObjectInputStream(socket.getInputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
+        }
+
+        @Override
+        public void run() {
+            //TODO
+            System.out.println("run");
+        }
+    }
 
 
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if(args.length <3 || args.length >4)
             throw new IllegalArgumentException("The number of arguments must be 3 or 4");
-
-        StorageNode sn = new StorageNode();
+        new StorageNode(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2]),args[3]);
     }
 }
